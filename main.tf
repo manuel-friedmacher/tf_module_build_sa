7a3b08c203d6a468c586051389ce35381a95b03e
Manuel Friedmacher <74181220+Friedmacher@users.noreply.github.com>
2026-07-29 19:37:10 +0200

    Update comments for clarity in main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "btp_subaccount" "subaccount" {
   }
 }
 
-# Establish trust to SAP Cloud Identity Provider (IDP)
+# Establish trust to SAP Cloud Identity Services (IDP)
 resource "btp_subaccount_trust_configuration" "create_trust" {
   subaccount_id            = btp_subaccount.subaccount.id
   identity_provider        = var.btp_idp
@@ -34,7 +34,7 @@ resource "btp_subaccount_trust_configuration" "create_trust" {
   status                   = "active"
 }
 
-# Assign the Subaccount Administrator role collection to the sa_administrators group in the subaccount
+# Assign the subaccount Administrator role collection to the sa_administrators group in the subaccount
 resource "btp_subaccount_role_collection_assignment" "sa_administrators" {
   subaccount_id        = btp_subaccount.subaccount.id
   origin               = var.btp_platform_idp
